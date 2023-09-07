58ee9709bd71e88ae2b1c4f39331ba345caf0b74
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2023-09-07 09:33:24 -0400

    le resultat de input ne s'affiche pas

--- jeux.js
+++ jeux.js
@@ -1,5 +1,30 @@
 let input = document.querySelector('#prix');
 let error = document.querySelector('small');
 let formulaire = document.querySelector('#formulaire');
+let bouton     = document.querySelector('button');
 input.textContent = 'bonjour';
 error.style.display = "none";
+// géneer un nombre aleatoire
+function genererNombreEntier(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+let nombreAleatoire = genererNombreEntier(1000);
+alert(nombreAleatoire);
+bouton.addEventListener('click', ()=>{
+   let  inputTest =  input.value;
+    if (inputTest == nombreAleatoire) {
+        alert('felicitation')
+        
+    } else if (inputTest < nombreAleatoire) {
+        error.style.disply= 'block';
+        error.textContent = 'moins'
+        }
+           else{
+            error.style.disply= 'block';
+           
+            error.textContent = "plus";
+           }{
+       
+        
+    }
+})
