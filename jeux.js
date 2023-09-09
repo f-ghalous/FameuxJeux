eec4b0cc2ac8e7b82273a889e3876606d04b5795
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2023-09-09 18:02:05 -0400

    la gestion des erreurs c bon elle est faite

--- jeux.js
+++ jeux.js
@@ -4,18 +4,28 @@ let formulaire = document.querySelector('#formulaire');
 let bouton     = document.querySelector('button');
 input.textContent = 'bonjour';
 error.style.display = "none";
-error.textContent = 'bbbbbbb';
+//error.textContent = 'bbbbbbb';
 
 // géneer un nombre aleatoire
 function genererNombreEntier(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+input.addEventListener('keyup', ()=>{
+    if (isNaN(input.value)) {
+        error.style.display = "inline";
+       
+    } else {
+        error.style.display = "none";
+   
+    }
+})
 
 bouton.addEventListener('click', ()=>{
     let  inputTest = parseInt(input.value);
    
-    let nombreAleatoire = genererNombreEntier(1000);
+    let nombreAleatoire = genererNombreEntier(1001);
     alert('je suis input  ' + inputTest + 'je suis aleatoire '+ nombreAleatoire);
+    
     if (inputTest == nombreAleatoire) {
         alert('felicitation')
         
